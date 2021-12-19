e607b6f52bc28a25bdba8a28751d362c038e00a5
amine1357 <akanemine123@gmail.com>
2021-12-19 07:15:40 +0000

    Update more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Helle feature");
+        System.out.println("GitHub Repo Published to Github");
+        System.out.println("Doing More Work");
 
     }
 
